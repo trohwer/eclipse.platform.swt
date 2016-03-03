ca5cf9a360ab880c3fa141d254b2545cd5202f58
Markus Keller <markus_keller@ch.ibm.com>
2016-03-03 22:07:23 +0100

    Bug 399786: fixed HiDPI layout for TrimBarLayout etc.

--- bundles/org.eclipse.swt/Eclipse SWT/win32/org/eclipse/swt/widgets/Composite.java
+++ bundles/org.eclipse.swt/Eclipse SWT/win32/org/eclipse/swt/widgets/Composite.java
@@ -236,7 +236,7 @@ public Point computeSizeInPixels (int wHint, int hHint, boolean changed) {
 		if (wHint == SWT.DEFAULT || hHint == SWT.DEFAULT) {
 			changed |= (state & LAYOUT_CHANGED) != 0;
 			state &= ~LAYOUT_CHANGED;
-			size = DPIUtil.autoScaleUp(layout.computeSize (this, wHint, hHint, changed));
+			size = DPIUtil.autoScaleUp(layout.computeSize (this, DPIUtil.autoScaleDown(wHint), DPIUtil.autoScaleDown(hHint), changed));
 		} else {
 			size = new Point (wHint, hHint);
 		}
