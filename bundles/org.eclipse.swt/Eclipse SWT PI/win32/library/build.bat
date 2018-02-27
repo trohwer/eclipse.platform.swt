f24c2a0567dea867f395992f2ac76bd3829c9a4e
Lakshmi Shanmugam <lshanmug@in.ibm.com>
2018-02-27 16:15:19 +0530

    local windows build changes
    Change-Id: Ief3afbd36560312bb535b947604c948fed8a676a

--- bundles/org.eclipse.swt/Eclipse SWT PI/win32/library/build.bat
+++ bundles/org.eclipse.swt/Eclipse SWT PI/win32/library/build.bat
@@ -11,7 +11,7 @@
 
 @echo off
 
-IF "x.%SWT_BUILDDIR%"=="x." set "SWT_BUILDDIR=W:"
+IF "x.%SWT_BUILDDIR%"=="x." set "SWT_BUILDDIR=C:\swtbuild"
 echo SWT build dir: %SWT_BUILDDIR%
 
 IF "x.%MSSDK%"=="x." set "MSSDK=%ProgramFiles(x86)%\Windows Kits\10"
@@ -20,7 +20,7 @@ IF "x.%WEBKIT_DIR%"=="x." set "WEBKIT_DIR=%SWT_BUILDDIR%\WebKit\r72896"
 echo Webkit dir: %WEBKIT_DIR%
 IF "x.%WEBKIT_SUPPORT_DIR%"=="x." set "WEBKIT_SUPPORT_DIR=%SWT_BUILDDIR%\WebKit\SupportLibrary"
 echo Webkit SupportLibrary dir: %WEBKIT_SUPPORT_DIR%
-IF "x.%MSVC_HOME%"=="x." set "MSVC_HOME=%SWT_BUILDDIR%\Microsoft\Visual Studio\2017\"
+IF "x.%MSVC_HOME%"=="x." set "MSVC_HOME=%SWT_BUILDDIR%\Microsoft Visual Studio\2017\"
 IF NOT EXIST "%MSVC_HOME%" set "MSVC_HOME=%ProgramFiles(x86)%\Microsoft Visual Studio\2017"
 echo Microsoft Visual Studio 2017 dir: %MSVC_HOME%
 
@@ -38,7 +38,7 @@ GOTO MAKE
 :X86_64
 set PROCESSOR_ARCHITECTURE=AMD64
 IF "x.%OUTPUT_DIR%"=="x." set OUTPUT_DIR=..\..\..\org.eclipse.swt.win32.win32.x86_64
-IF "x.%SWT_JAVA_HOME%"=="x." set "SWT_JAVA_HOME=%SWT_BUILDDIR%\Java\Oracle\jdk1.8.0-latest\x64"
+IF "x.%SWT_JAVA_HOME%"=="x." set "SWT_JAVA_HOME="C:\swtbuild\jdk1.8.0_51"
 echo SWT_JAVA_HOME x64: %SWT_JAVA_HOME%
 set CFLAGS=-DJNI64
 call "%MSVC_HOME%\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
